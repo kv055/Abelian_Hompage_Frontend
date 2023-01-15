910b6ef8425b7e8f2d3b6d2eaf024d960abb49ed
Kilian Voss <kilian96@live.de>
2023-01-15 15:42:18 +1100

    Tailwind works, and first draft of the site is finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import './index.css'
+import React from "react";
+
+// import './App.css'
+import Logo from './assets/logo.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-white h-screen flex flex-col items-center justify-start m-10">
+      <div className="">
+        <img src={Logo} alt="Logo" className="w-1/2 md:w-1/2 lg:w-1/2 mx-auto mb-6 shadow-2xl shadow-violet-900" />
       </div>
-      <h1 className='text-red-800' >Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav className="flex flex-row flex-wrap m-20">
+        <a href="#" className="RobotoCondensed hover:underline mr-6 ml-6 text-lg cursor-pointer text-indigo-700">
+          About Us
+        </a>
+        <a href="#" className="RobotoCondensed hover:underline mr-6 ml-6 text-lg cursor-pointer text-indigo-700">
+          Methodology/Vision
+        </a>
+        <a href="#" className="RobotoCondensed hover:underline mr-6 ml-6 text-lg cursor-pointer text-indigo-700">
+          User Login
+        </a>
+        <a href="#" className="RobotoCondensed hover:underline mr-6 ml-6 text-lg cursor-pointer text-indigo-700">
+          Terminal Demo
+        </a>
+      </nav>
     </div>
-  )
+  );
 }
+// There are a few other possibilities that could be causing the w-1/2 and h-1/2 classes not to work as expected:
 
-export default App
+// The w-1/2 and h-1/2 classes are not being imported into your CSS file. Make sure that you are importing the correct CSS file that contains the Tailwind classes, and that the file is correctly linked to your HTML file.
+
+// There is another CSS class or style that is overriding the w-1/2 and h-1/2 classes. Make sure that the img element does not have any CSS styles applied to it that might be conflicting with the Tailwind classes.
+
+// The image path passed to the src attribute might be incorrect. Make sure the path to the image is correctly pointing to the image file
+
+// The version of the tailwindcss you are using doesn't support the responsive width you set. check the version that you are using and check their documentations if the class w-1/2 exist or not
+
+// I recommend double-checking that the Tailwind CSS file is imported correctly, and making sure that the img element does not have any conflicting CSS styles. You can also check the path of the image and its existence to see if it is available and loaded.
+
+// Let me know if you need any more help, I'm happy to assist.
+
+
+
+
+
+
+export default App;
