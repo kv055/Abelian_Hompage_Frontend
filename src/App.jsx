c379ef43ed3031ab7eca98fb2d78546f1ba4c14a
Kilian Voss <kilian96@live.de>
2023-01-25 15:30:29 +1100

    Solved the rendering problem by rendering a function that gets the selectedsite as input and returns the module to render.

--- src/App.jsx
+++ src/App.jsx
@@ -8,47 +8,41 @@ import About_Us_Page from "./Sites/About";
 import Methodology_Vision_Page from "./Sites/Methodology_Vision";
 import User_Login_Page from "./Sites/User_Login";
 
-
-
 function App() {
-  const [Selected_Site, set_Selected_Site] = React.useState('Landing_Page');
-  
-  
-  let Site_to_render = Landing_Page
+  const [Selected_Site, set_Selected_Site] = React.useState('Landing_Page')
 
-  React.useEffect(() => {
-    // Code to execute when Selected_Site changes
-    console.log(Selected_Site);
-
-    switch (Selected_Site) {
+  function Rendering_function(Site_Name){
+    switch (Site_Name) {
       case 'Landing_Page':
-        Site_to_render = Landing_Page;
         console.log('Landing_Page');
+        return Landing_Page
         break;
       case 'About_Page':
-        Site_to_render = About_Us_Page;
         console.log('About_Page');
+        return About_Us_Page
         break;
       case 'Methodology_Vision_Page': 
-        Site_to_render = Methodology_Vision_Page;
-        console.log('Methology_Vision_Page');
+        console.log('Methology_Vision_Page'); 
+        return Methodology_Vision_Page
         break;
       case 'User_Login_Page':
-        Site_to_render = User_Login_Page;
         console.log('User_Login_Page');
+        return User_Login_Page
         break;
       case 'Terminal_Demo_Page':
-        Site_to_render = <p>In developement</p>;
         console.log('Terminal_Demo_Page');
+        return <h1>In developement</h1>
         break;
-      // default:
-      //   Site_to_render = Landing_Page;
+      default:
+        return Landing_Page
     }
-  }, [Selected_Site]); 
-  let L = 0
+  }
+  
+  let To_Render = Rendering_function(Selected_Site)
+
   return (
     <Selected_Site_Context.Provider value={{Selected_Site, set_Selected_Site}}>
-      <Site_to_render />
+      <To_Render />
     </Selected_Site_Context.Provider>
   );
 }
