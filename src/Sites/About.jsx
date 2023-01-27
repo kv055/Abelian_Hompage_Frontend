f91870f6a91e75147f55b235173a9e98b9f52acf
Kilian Voss <kilian96@live.de>
2023-01-27 13:42:15 +1100

    Changes in the about page and Kilian Face photo for about page

--- src/Sites/About.jsx
+++ src/Sites/About.jsx
@@ -1,50 +1,73 @@
 import React from 'react';
 import { Selected_Site_Context } from '../context_test';
 import Logo from '../assets/logo.png'
+import Kilian from '../assets/Passfoto.jpg'
 
 const About_Us_Page = () => {
     const {Selected_Site, set_Selected_Site} = React.useContext(Selected_Site_Context)
 
-  return (
-    <div>
-        <div className="">
-            <img 
-                src={Logo} 
-                alt="Logo" 
-                className="w-1/3 md:w-1/3 lg:w-1/3 mx-auto mb-6 shadow-2xl shadow-violet-900"
-                onClick={() => set_Selected_Site("Landing Page")}
-            />
-            <h1 className='RobotoCondensed'>About Us</h1>
-        </div>
+    return (
         <div>
-            <h3 className='RobotoCondensed'>The Team</h3>
-            <div>
-                <h5 className='RobotoCondensed'>William Godfrey</h5>
-                <img />
-                <p className='RobotoCondensed'>
-
-                </p>
+            <div className="">
+                <img 
+                    src={Logo} 
+                    alt="Logo" 
+                    className="w-1/3 md:w-1/3 lg:w-1/3 mx-auto mb-6 shadow-2xl shadow-violet-900"
+                    onClick={() => set_Selected_Site("Landing Page")}
+                />
+                <h1 className='RobotoCondensed'>About Us</h1>
             </div>
-            <div>
-                <h5 className='RobotoCondensed'>Kilian Voss</h5>
-                <img />
-                <p className='RobotoCondensed'>
+            <div className='flex flex-col justify-center'>
+                <h3 className='RobotoCondensed'>The Team</h3>
+                <div className='flex flex-col'>
+                    <div className='flex flex-col'>
+                        <h5 className='RobotoCondensed'>William Godfrey</h5>
+                        <h6 className='RobotoCondensed'>Role</h6>
+                        <img 
+                            src={Logo} 
+                            alt="Logo" 
+                            className="w-1/3 md:w-1/3 lg:w-1/3 mx-auto mb-6 shadow-2xl shadow-violet-900"
+                        />
+                        <p className='RobotoCondensed'>
+    
+                        </p>
+                    </div>
+                    <div className='flex flex-col justify-center'>
+                        <h5 className='RobotoCondensed justify-center'>Kilian Voss</h5>
+                        <h6 className='RobotoCondensed'>Role</h6>
+                        <img 
+                            src={Logo} 
+                            alt="Logo" 
+                            className="w-1/3 md:w-1/3 lg:w-1/3 mx-auto mb-6 shadow-2xl shadow-violet-900"
+                        />
+                        <p className='RobotoCondensed'>
+    
+                        </p>
+                    </div>
+                    <div className='flex flex-col'>
+                        <h5 className='RobotoCondensed'>Lachlan Steele</h5>
+                        <h6 className='RobotoCondensed'>Role</h6>
+                        <img 
+                            src={Logo} 
+                            alt="Logo" 
+                            className="w-1/3 md:w-1/3 lg:w-1/3 mx-auto mb-6 shadow-2xl shadow-violet-900"
+                        />
+                        <p className='RobotoCondensed'>
+    
+                        </p>
+                    </div>
+                    <div className='flex flex-col'>
+                        <h5 className='RobotoCondensed'>Fortino Gomez</h5>
+                        <h6 className='RobotoCondensed'>Role</h6>
+                        <img 
+                            src={Logo} 
+                            alt="Logo" 
+                            className="w-1/3 md:w-1/3 lg:w-1/3 mx-auto mb-6 shadow-2xl shadow-violet-900"
+                        />
+                    <p className='RobotoCondensed'>
 
-                </p>
-            </div>
-            <div>
-                <h5 className='RobotoCondensed'>Lachlan Steele</h5>
-                <img />
-                <p className='RobotoCondensed'>
-
-                </p>
-            </div>
-            <div>
-                <h5 className='RobotoCondensed'>Fortino Gomez</h5>
-                <img />
-                <p className='RobotoCondensed'>
-                    
-                </p>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
