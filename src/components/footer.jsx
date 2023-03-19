4213a382698f4d515d3909fd26de3b87cf80deab
Kilian Voss <kilian96@live.de>
2023-03-19 15:56:14 +1100

    ©

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const header = () => {
   return (
     <div className="bg-white p-5 flex">
-        <p className='RobotoCondensed '>Abelian Capital 2023</p>
+        <p className='RobotoCondensed '> © Abelian Capital 2023</p>
     </div>
   )
 }
